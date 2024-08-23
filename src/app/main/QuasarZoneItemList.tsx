7daebf3be7005d2bd1194b6e17e32a280ee603e7
nhk9710 <hknoh@wkit.co.kr>
2024-08-23 17:16:29 +0900

    [feat]main페이지 ui변경

--- src/app/main/QuasarZoneItemList.tsx
+++ src/app/main/QuasarZoneItemList.tsx
@@ -3,12 +3,10 @@ import ItemList, {Item} from "@/component/ItemList";
 import {Suspense} from "react";
 import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
-import Image from "next/image";
-import {Badge} from "@/components/ui/badge";
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 export default async function QuasarZoneItemList() {
-    // const imageUrl = "https://image.fmkorea.com/logos/fmkorealogo_h1.png"
-    const imageUrl = ""
+    const imageUrl = "https://image.fmkorea.com/logos/fmkorealogo_h1.png"
 
     return (
         <Card>
@@ -19,6 +17,7 @@ export default async function QuasarZoneItemList() {
                 </CardDescription>
             </CardHeader>
             <CardContent>
+                <ScrollArea style={{ height: '60vh' }}>
                 <Table>
                     <TableHeader>
                         <TableRow>
@@ -28,104 +27,15 @@ export default async function QuasarZoneItemList() {
                             <TableHead>Name</TableHead>
                             <TableHead>Status</TableHead>
                             <TableHead className="hidden md:table-cell">Price</TableHead>
-                            <TableHead className="hidden md:table-cell">
-                                Total Sales
-                            </TableHead>
-                            <TableHead className="hidden md:table-cell">Created at</TableHead>
-                            <TableHead>
-                                <span className="sr-only">Actions</span>
-                            </TableHead>
                         </TableRow>
                     </TableHeader>
                     <TableBody>
-                        <TableRow>
-                            <TableCell className="hidden sm:table-cell">
-                                <Image
-                                    alt="Product image"
-                                    className="aspect-square rounded-md object-cover"
-                                    height="64"
-                                    src="/placeholder.svg"
-                                    width="64"
-                                />
-                            </TableCell>
-                            <TableCell className="font-medium">
-                                Laser Lemonade Machine
-                            </TableCell>
-                            <TableCell>
-                                <Badge variant="outline">Draft</Badge>
-                            </TableCell>
-                            <TableCell className="hidden md:table-cell">$499.99</TableCell>
-                            <TableCell className="hidden md:table-cell">25</TableCell>
-                            <TableCell className="hidden md:table-cell">
-                                2023-07-12 10:42 AM
-                            </TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell className="hidden sm:table-cell">
-                                <Image
-                                    alt="Product image"
-                                    className="aspect-square rounded-md object-cover"
-                                    height="64"
-                                    src="/placeholder.svg"
-                                    width="64"
-                                />
-                            </TableCell>
-                            <TableCell className="font-medium">
-                                Hypernova Headphones
-                            </TableCell>
-                            <TableCell>
-                                <Badge variant="outline">Active</Badge>
-                            </TableCell>
-                            <TableCell className="hidden md:table-cell">$129.99</TableCell>
-                            <TableCell className="hidden md:table-cell">100</TableCell>
-                            <TableCell className="hidden md:table-cell">
-                                2023-10-18 03:21 PM
-                            </TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell className="hidden sm:table-cell">
-                                <Image
-                                    alt="Product image"
-                                    className="aspect-square rounded-md object-cover"
-                                    height="64"
-                                    src="/placeholder.svg"
-                                    width="64"
-                                />
-                            </TableCell>
-                            <TableCell className="font-medium">AeroGlow Desk Lamp</TableCell>
-                            <TableCell>
-                                <Badge variant="outline">Active</Badge>
-                            </TableCell>
-                            <TableCell className="hidden md:table-cell">$39.99</TableCell>
-                            <TableCell className="hidden md:table-cell">50</TableCell>
-                            <TableCell className="hidden md:table-cell">
-                                2023-11-29 08:15 AM
-                            </TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell className="hidden sm:table-cell">
-                                <Image
-                                    alt="Product image"
-                                    className="aspect-square rounded-md object-cover"
-                                    height="64"
-                                    src="/placeholder.svg"
-                                    width="64"
-                                />
-                            </TableCell>
-                            <TableCell className="font-medium">
-                                TechTonic Energy Drink
-                            </TableCell>
-                            <TableCell>
-                                <Badge variant="secondary">Draft</Badge>
-                            </TableCell>
-                            <TableCell className="hidden md:table-cell">$2.99</TableCell>
-                            <TableCell className="hidden md:table-cell">0</TableCell>
-                            <TableCell className="hidden md:table-cell">
-                                2023-12-25 11:59 PM
-                            </TableCell>
-                        </TableRow>
+                        <Suspense fallback={<div>Loading ...</div>}>
+                            <ItemList items={await getQuasarZoneItem()} imageUrl={imageUrl}/>
+                        </Suspense>
                     </TableBody>
                 </Table>
+                </ScrollArea>
             </CardContent>
             <CardFooter>
                 <div className="text-xs text-muted-foreground">
@@ -133,9 +43,6 @@ export default async function QuasarZoneItemList() {
                 </div>
             </CardFooter>
         </Card>
-        // <Suspense fallback={<div>Loading ...</div>}>
-        //     <ItemList items={await getQuasarZoneItem()} imageUrl={imageUrl}/>
-        // </Suspense>
     )
 }
 const getQuasarZoneItem = async () => {
@@ -152,7 +59,7 @@ const parsingItem = (item: HTMLElement): Item => {
     // @ts-ignore
     return {
         title: item.querySelector("span.ellipsis-with-reply-cnt")?.textContent,
-        // image: item.querySelector("img")?.attributes.src,
+        image: item.querySelector("img")?.attributes.src,
         price: item.querySelector("span.text-orange")?.textContent || '?',
         url: baseUrl + item.querySelector("a")?.attributes.href || '',
         voted: item.querySelector("span.ctn-count")?.textContent || '0',
--- src/component/ItemList.tsx
+++ src/component/ItemList.tsx
@@ -1,31 +1,49 @@
 'use client';
 import {Key} from "react";
 import styled from "./ItemList.module.css"
+import {
+    TableCell,
+    TableRow,
+} from "@/components/ui/table"
+import Image from "next/image";
+import {Badge} from "@/components/ui/badge";
 
 export default function ItemList(props : {items:Item[],imageUrl:string})  {
 
+    const addHttps = (url: string | undefined) => {
+        if(url){
+        if(!url.startsWith('https:')){
+            return 'https:' + url
+        }
+        return url
+        }else{
+            return ''
+        }
+    }
     const linkTo = (url: string) => url ? window.location.href = url : alert('유효하지 않은 링크 입니다')
 
     return (
         <>
-            <div className={styled.item_wrapper}>
-                <img className={styled.icon} src={props.imageUrl} alt={"image"}/>
-                <ul>
-                    {props.items.map((item: Item, i: Key | null | undefined) =>
-                        <li className={styled.item_li} key={i}>
-                            <div className={styled.item} onClick={() => linkTo(item.url)}>
-                                <img className={styled.item_img} src={item.image} alt={item.title}/>
-                                <div className={styled.item_title}>
-                                    <p>{item.title}[{item.voted}]</p>
-                                    <p className="item-voted">조회수 {item.voted}</p>
-                                    <p>{item.price}</p>
-                                </div>
-                            </div>
-
-                        </li>
-                    )}
-                </ul>
-            </div>
+            {props.items.map((item: Item, i: Key | null | undefined) =>
+            <TableRow key={i}>
+                <TableCell className="hidden sm:table-cell">
+                    <Image
+                        alt="Product image"
+                        className="aspect-square rounded-md object-cover"
+                        height="64"
+                        src={addHttps(item.image)}
+                        width="64"
+                    />
+                </TableCell>
+                <TableCell className="font-medium">
+                    {item.title}[{item.voted}]
+                </TableCell>
+                <TableCell>
+                    <Badge variant="outline">{item.voted}</Badge>
+                </TableCell>
+                <TableCell className="hidden md:table-cell">{item.price}</TableCell>
+            </TableRow>
+            )}
         </>
     )
 }
