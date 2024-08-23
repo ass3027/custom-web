227e42394606b5574d5200765b36ea6fbe97122b
nhk9710 <hknoh@wkit.co.kr>
2024-08-23 11:22:41 +0900

    [feat]Header,bord 업데이트

--- src/app/main/board/page.tsx
+++ src/app/main/board/page.tsx
@@ -1,7 +1,66 @@
+"use client";
+import { CardContent, Card, CardTitle, CardDescription, CardFooter, CardHeader } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+} from "@/components/ui/carousel"
+import Autoplay from "embla-carousel-autoplay";
+import * as React from 'react'
+
+
 export default function Board() {
+    const plugin = React.useRef(
+        Autoplay({ delay: 2000, stopOnInteraction: true })
+    )
+
+    const changeNum = (num:number) => {
+        switch (num){
+            case 1:
+                return "Uno";
+            case 2:
+                return "Dos";
+            case 3:
+                return "Tres";
+            case 4:
+                return "Cuatro";
+            case 5:
+                return "Cinco";
+        }
+    }
+
     return (
-        <div>
-            <h1>BOARD</h1>
+        <div className="w-screen flex justify-center items-center" style={{height: '80vh'}}>
+            <div className="border rounded w-4/5 h-4/5 shadow-2xl">
+                <h1>BOARD</h1>
+                <div>
+                    <h2>hello</h2>
+                </div>
+                <Separator />
+                <div className="border-b-blue-600 border">
+                    <Carousel
+                        plugins={[plugin.current]}
+                        className="w-40"
+                        onMouseEnter={plugin.current.stop}
+                        onMouseLeave={plugin.current.reset}
+                    >
+                        <CarouselContent>
+                            {Array.from({ length: 5 }).map((_, index) => (
+                                <CarouselItem key={index}>
+                                    <div className="p-1">
+                                        <Card>
+                                            <CardContent className="flex aspect-square items-center justify-center p-6">
+                                                <span className="text-4xl font-semibold">{changeNum(index + 1)}</span>
+                                            </CardContent>
+                                        </Card>
+                                    </div>
+                                </CarouselItem>
+                            ))}
+                        </CarouselContent>
+                    </Carousel>
+                </div>
+            </div>
         </div>
     )
 }
--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,9 +1,20 @@
 import Link from 'next/link';
 import { Button } from '@/components/ui/button'
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+
+
 
 export default function Header() {
     return (
-        <div className="p-10 border-b border-slate-100">
+        <div className="p-10 flex justify-between border-b border-slate-100">
+            <Button>LOGO</Button>
             <nav className="flex justify-center gap-x-4 text-indigo-500 font-bold">
                 <Link href="/">
                     <Button>HOME</Button>
@@ -18,6 +29,17 @@ export default function Header() {
                     <Button>DETAIL</Button>
                 </Link>
             </nav>
+            <Sheet>
+                <SheetTrigger><Button>Open</Button></SheetTrigger>
+                <SheetContent>
+                    <SheetHeader>
+                        <SheetTitle>Who is the best soccer player in the world?</SheetTitle>
+                        <SheetDescription>
+                            I think the best soccer player is Messi.
+                        </SheetDescription>
+                    </SheetHeader>
+                </SheetContent>
+            </Sheet>
         </div>
     )
 }
